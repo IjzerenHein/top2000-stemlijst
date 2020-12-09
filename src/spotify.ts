97a4a345de10f46d2dd257ae24ee4566b5b0285f
Hein Rutjes <hrutjes@gmail.com>
2020-12-09 15:13:26 +0100

    fix redirect uri

--- src/spotify.ts
+++ src/spotify.ts
@@ -19,7 +19,7 @@ export function authorizeSpotify(importId: string, isPublicPlaylist: boolean) {
   const redirectUri =
     process.env.NODE_ENV === "development"
       ? "http://localhost:19006/authorize"
-      : "https://www.spotify-import/authorize";
+      : "https://www.spotify-import.com/authorize";
   const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${redirectUri}&state=${importId}&scope=${
     isPublicPlaylist ? "playlist-modify-private" : "playlist-modify-public"
   }`;
